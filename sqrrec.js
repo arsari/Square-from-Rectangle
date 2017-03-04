61a24cb39f4e6a715b77e855c15228a20df01eff
Arturo Santiago-Rivera <asantiago@arsari.com>
2017-03-04 00:22:28 -0400

    Code stability and add color.

--- sqrrec.js
+++ sqrrec.js
@@ -1,5 +1,6 @@
 //  node terminal prompt-sync package require
 var prompt = require('prompt-sync')();
+var colors = require('colors');
 
 // object class function declaration
 function Figure(length, height) {
@@ -12,43 +13,53 @@ function Figure(length, height) {
         return (this.length + this.height) * 2;
     };
     this.draw = function() {
-        var tbRow = '##'.repeat(this.length);
+        var tbRow = '  '.repeat(this.length);
         var mRow = '  '.repeat(this.length - 1);
-        console.log(tbRow);
-        for (var i = 0; i < this.height - 2; i++) {
-            console.log(tbRow[0] + mRow + tbRow[this.length]);
+        if (length === height) {
+          console.log(tbRow.bgGreen);
+          for (var i = 0; i < this.height - 2; i++) {
+              console.log(tbRow[0].bgGreen + mRow + tbRow[this.length].bgGreen);
+          }
+          console.log(tbRow.bgGreen);
+        } else {
+          console.log(tbRow.bgCyan);
+          for (var i = 0; i < this.height - 2; i++) {
+              console.log(tbRow[0].bgCyan + mRow + tbRow[this.length].bgCyan);
+          }
+          console.log(tbRow.bgCyan);
         }
-        console.log(tbRow);
         return '';
     };
 }
 
 // input loop
 while (true) {
-    // user input prompt
-    console.log('==========================================================');
-    console.log('This script will draw and calculate the area and perimeter\nof a rectangle and them will draw and calculate the area\nand perimeter of a square using on the following inputs...\n(For exit, enter 0 in any of the inputs.)');
-    console.log('----------------------------------------------------------');
-    var userLength = Number(prompt('Enter a LENGTH value for the Rectangle: '));
-    var userHeight = Number(prompt('Enter a HEIGHT value for the Rectangle: '));
-    console.log('==========================================================\n');
+    // script description and user input prompt
+    console.log('\n=============================================='.red + 'v1.3.170304'.yellow);
+    console.log('This script will draw and calculate the area and perimeter\nof a rectangle and them will draw and calculate the area\nand perimeter of a square using on the following inputs...\n(For exit, press Enter in any of the inputs.)'.cyan);
+    console.log('----------------------------------------------------------'.blue);
+    var userLength = Number(prompt('Enter a LENGTH value for the Rectangle: '.italic.yellow));
+    var userHeight = Number(prompt('Enter a HEIGHT value for the Rectangle: '.italic.yellow));
+    console.log('==========================================================\n'.red);
 
     // user inputs evaluation
-    if (userLength === 0 || userHeight === 0) {
-      console.log('################');
-      console.log('# Thank You!!! #');
-      console.log('################\n');
+    if (isNaN(userLength) || isNaN(userHeight)) {
+      console.log('*** INPUT ERROR ***'.bgRed + '\n' + 'Input value of Length and Height should be a Number.'.bgRed);
+    } else if (userLength === 0 || userHeight === 0) {
+      console.log('-----------------'.bgMagenta);
+      console.log('|'.bgMagenta + '  Good Bye!!!  ' + '|'.bgMagenta);
+      console.log('-----------------'.bgMagenta + '\n');
       return;
-    } else if (userLength < 3 || userLength === 'NaN' || userHeight < 3 || userHeight === 'NaN') {
-        console.log('*** INPUT ERROR ***\nInput value of Length and Height should not be empty or less than 3 units.\n');
+    } else if (userLength < 3 || userHeight < 3 ) {
+        console.log('*** INPUT ERROR ***'.bgRed + '\n' + 'Input value of Length and Height should not be less than 3 units.'.bgRed);
     } else {
         // call Figure() class function for rectangle calc
         var userRect = new Figure(userLength, userHeight);
 
         // rectangle outputs
         userRect.draw();
-        console.log('\nRectangle total area is ' + userRect.getArea() + ' sq. units.');
-        console.log('Rectangle perimeter length is ' + userRect.getPerimeter() + ' units.\n');
+        console.log('\nRectangle total area is '.bold.yellow + userRect.getArea() + ' sq. units.'.bold.yellow);
+        console.log('Rectangle perimeter length is '.bold.yellow + userRect.getPerimeter() + ' units.\n'.bold.yellow);
 
         // determine square sides length
         var sqrSide = Math.floor(userRect.getPerimeter() / 4);
@@ -58,8 +69,8 @@ while (true) {
 
         // square outputs
         userSqr.draw();
-        console.log('\nSquare total area is ' + userSqr.getArea() + ' sq. units.');
-        console.log('Square perimeter length is ' + userSqr.getPerimeter() + ' units.\n');
+        console.log('\nSquare total area is '.bold.yellow + userSqr.getArea() + ' sq. units.'.bold.yellow);
+        console.log('Square perimeter length is '.bold.yellow + userSqr.getPerimeter() + ' units.\n'.bold.yellow);
         return;
     }
 }
--- sqrrec.py
+++ sqrrec.py
@@ -1,5 +1,6 @@
 # encoding declaration
 """coding=utf-8"""
+from colorama import Fore, Back, Style
 
 
 # area function definition
@@ -14,44 +15,51 @@ def getPerimeter(userLength, userHeight):
 
 # draw function definition
 def draw(userLength, userHeight):
-    tbRow = '##' * userLength
+    tbRow = '  ' * userLength
     mRow = '  ' * (userLength - 1)
     i = 0
-    print(tbRow)
-    while i < (userHeight - 2):
-        print(tbRow[0] + mRow + tbRow[-1])
-        i += 1
-    print(tbRow)
+    if userLength == userHeight:
+        print(Back.GREEN + tbRow + Back.RESET)
+        while i < (userHeight - 2):
+            print(Back.GREEN + tbRow[0] + Back.RESET + mRow + Back.GREEN + tbRow[-1] + Back.RESET)
+            i += 1
+        print(Back.GREEN + tbRow + Back.RESET)
+    else:
+        print(Back.CYAN + tbRow + Back.RESET)
+        while i < (userHeight - 2):
+            print(Back.CYAN + tbRow[0] + Back.RESET + mRow + Back.CYAN + tbRow[-1] + Back.RESET)
+            i += 1
+        print(Back.CYAN + tbRow + Back.RESET)
     return ''
 
 
 # input loop
 while True:
-    # introduction
-    print('==========================================================')
-    print('This script will draw and calculate the area and perimeter\nof a rectangle and them will draw and calculate the area\nand perimeter of a square using on the following inputs...\n(For exit, enter 0 in any of the inputs.)')
-    print('----------------------------------------------------------')
-    # user input prompt
+    # script description
+    print(Fore.RED + '\n==============================================' + Fore.YELLOW + 'v1.3.170304' + Fore.RESET)
+    print(Fore.CYAN + 'This script will draw and calculate the area and perimeter\nof a rectangle and them will draw and calculate the area\nand perimeter of a square using on the following inputs...\n(For exit, enter 0 in any of the inputs.)' + Fore.RESET)
+    print(Fore.BLUE + '----------------------------------------------------------' + Fore.RESET)
+    # user input prompt and evaluation
     try:
-        userLength = int(input('Enter a LENGTH value for the Rectangle: '))
-        userHeight = int(input('Enter a HEIGHT value for the Rectangle: '))
+        userLength = int(input(Fore.YELLOW + 'Enter a LENGTH value for the Rectangle: ' + Fore.RESET))
+        userHeight = int(input(Fore.YELLOW + 'Enter a HEIGHT value for the Rectangle: ' + Fore.RESET))
     except Exception:
-        print('\n*** INPUT ERROR ***\nInput value of Length and Height should be a Number and not empty.\n')
+        print('\n' + Back.RED + '*** INPUT ERROR ***' + Back.RESET + '\n' + Back.RED + 'Input value of Length and Height should be a Number.' + Back.RESET)
         continue
-    print('==========================================================\n')
+    print(Fore.RED + '==========================================================\n' + Fore.RESET)
 
     # user input evaluation
     if userLength == 0 or userHeight == 0:
-        print('################')
-        print('# Thank You!!! #')
-        print('################\n')
+        print(Back.MAGENTA + '-----------------' + Back.RESET)
+        print(Back.MAGENTA + '|' + Back.RESET + '  Good Bye!!!  ' + Back.MAGENTA + '|' + Back.RESET)
+        print(Back.MAGENTA + '-----------------' + Back.RESET + '\n')
         break
     elif userLength < 3 or userHeight < 3:
-        print('*** INPUT ERROR ***\nInput value of Length and Height should not be less than 3 units.\n')
+        print(Back.RED + '*** INPUT ERROR ***' + Back.RESET + '\n' + Back.RED + 'Input value of Length and Height should not be less than 3 units.' + Back.RESET)
     else:
         # rectangle outputs
         draw(userLength, userHeight)
-        print('\nRectangle total area is ' + str(getArea(userLength, userHeight)) + ' sq. units.')
+        print('\n' + Style.BRIGHT + Fore.YELLOW + 'Rectangle total area is ' + str(getArea(userLength, userHeight)) + ' sq. units.')
         print('Rectangle perimeter length is ' + str(getPerimeter(userLength, userHeight)) + ' units.\n')
 
         # determine square sides length
